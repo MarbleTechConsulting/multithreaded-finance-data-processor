5cfda7050cc8bb3fc84110096c8d981f9fdc1c76
Llywelyn OWEN <owen.perso@outlook.com>
2024-07-16 14:53:27 +0000

    Mfdp 003

--- MultithreadedFinanceDataProcessor/obj/Debug/net8.0/MultithreadedFinanceDataProcessor.AssemblyInfo.cs
+++ MultithreadedFinanceDataProcessor/obj/Debug/net8.0/MultithreadedFinanceDataProcessor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultithreadedFinanceDataProcessor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15659b48a9c054b982f080a1ff5f1006bb95d4db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba525ea15004488d1a4cf1cfbb889d328c2ba800")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultithreadedFinanceDataProcessor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultithreadedFinanceDataProcessor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
